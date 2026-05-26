9610111a1ebeb5cd55e5ef181ec6e578c124a764
kevinAguilar13 <kevinaguilar131104@gmail.com>
2026-05-25 22:30:39 -0500

    subiendo-proyecto

---
+++ src/main/java/com/chefcontrol/ChefcontrolApplication.java
@@ -0,0 +1,13 @@
+package com.chefcontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChefcontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChefcontrolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chefcontrol/ChefcontrolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chefcontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChefcontrolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
